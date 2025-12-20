b1390cc80864872f5c9bce565b29789f1768b667
Clement Mathole <Clementmathole003@gmail.com>
2025-12-20 19:32:58 +0200

    Fix typo in Home.jsx description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
                     <img src={meImage} alt="Clement Mathole" width={100}/>
                     <p>Hi I'm Clement</p>
                     <h1>Full Stack <span className="gradient-text">Software</span> <br /> Developer/ <span className="gradient-text">Engineer</span> </h1>
-                    <p>I design, build, and deploy seamless digital experiences brusing modern technologies. From responsive <br />front-ends to efficient back-end systems, I bring ideas to life through clean code and intuitive design.</p>
+                    <p>I design, build, and deploy seamless digital experiences using modern technologies. From responsive <br />front-ends to efficient back-end systems, I bring ideas to life through clean code and intuitive design.</p>
                     <div className="action-btns">
                         <button id='my-work' onClick={() => navigate("/portfolio")}>My Work</button>
                         <button id='l-connect' onClick={() => navigate("/contact")}>Let’s Connect <img src={emailIcon} alt="mail icon" width={18}/></button>
